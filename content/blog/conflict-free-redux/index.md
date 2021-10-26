05c430e867192fd59402a4ac19310f7fb8590928
Michael L Perry <michael@qedcode.com>
2021-10-26 07:07:01 -0500

    Removed sample content

---
+++ content/blog/conflict-free-redux/index.md
@@ -0,0 +1,5 @@
+---
+title: Conflict-Free Redux
+date: "2021-10-26T11:53:37Z"
+description: "Make Redux converge when multiple users apply changes concurrently."
+---
--- src/components/layout.js
+++ src/components/layout.js
@@ -11,9 +11,7 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header"><Header /></header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        © {new Date().getFullYear()}, Michael L Perry
       </footer>
     </div>
   )
